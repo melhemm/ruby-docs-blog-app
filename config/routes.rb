0c202276a7398a46a72462bec7d918b636bfb115
melhemm <jamalmulhemm@gmail.com>
2021-11-09 23:17:19 +0300

    Refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
-  # , to: "articles#index"
-  # resources :articles
-  # , to: "articles#show"
+  resources :articles do
+   resources :comments
+  end
 end
